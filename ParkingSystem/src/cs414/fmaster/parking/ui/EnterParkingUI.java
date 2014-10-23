732e76fb7759b9b32fe33bb96e46cec9e7c0947d
masterf89 <farheenmaster@gmail.com>
2014-10-23 12:04:37 -0400

    EnterParking UI Handler

--- ParkingSystem/src/cs414/fmaster/parking/ui/EnterParkingUI.java
+++ ParkingSystem/src/cs414/fmaster/parking/ui/EnterParkingUI.java
@@ -3,7 +3,6 @@
  */
 package cs414.fmaster.parking.ui;
 
-import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -12,55 +11,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-import cs414.fmaster.parking.ParkingHandler;
-import cs414.fmaster.parking.ParkingRate;
-import cs414.fmaster.parking.ParkingSystem;
+import cs414.fmaster.parking.controller.MainController;
+import cs414.fmaster.parking.controller.ParkingRate;
+import cs414.fmaster.parking.controller.ParkingOperationsController;
 
 /**
  * @author MasterF
  * 
  */
 public class EnterParkingUI {
-	ParkingSystem ps;
-	ParkingUI mainUI;
-	JPanel enterParkingMainPanel = new JPanel();
-	JPanel parkingRatesPanel = new JPanel();
+	MainUI mainUI;
+	MainController mainController;
+	JPanel mainContentPnl = new JPanel();
 	JButton getTicketBtn = new JButton("Get Ticket");
+	JButton backButton = new JButton("Go Back");
 
 	private static EnterParkingUI instance = null;
 
-	protected EnterParkingUI(ParkingSystem ps, ParkingUI mainUI) {
-		super();
-		this.ps = ps;
+	private EnterParkingUI(MainUI mainUI, MainController mainController) {
 		this.mainUI = mainUI;
+		this.mainController = mainController;
 	}
 
-	public static EnterParkingUI getInstance(ParkingSystem ps, ParkingUI mainUI) {
+	public static EnterParkingUI getInstance(MainUI mainUI, MainController mainController) {
 		if (instance == null) {
-			instance = new EnterParkingUI(ps, mainUI);
+			instance = new EnterParkingUI(mainUI, mainController);
 		}
 		return instance;
 	}
 
-	public void generateEnterParkingScreen() {
-		enterParkingMainPanel.setLayout(new GridBagLayout());
-		displayParkingRates();
-		getTicketBtn.addActionListener(new EnterParkingListener());
-		mainUI.addComponent(enterParkingMainPanel, getTicketBtn, GridBagConstraints.NONE, 0, 1);
-		mainUI.addComponent(mainUI.getMainPanel(), enterParkingMainPanel, GridBagConstraints.BOTH, 0, 0);
-		mainUI.showContentPanel(enterParkingMainPanel);
-		mainUI.setMessage("");
-	}
+	public void setupUI() {
+		mainContentPnl.setLayout(new GridBagLayout());
 
-	private void displayParkingRates() {
+		JPanel parkingRatesPanel = new JPanel();
+		parkingRatesPanel.setLayout(new GridBagLayout());
+		JLabel parkingRateLbl = new JLabel("Current Parking Rates");
+		mainUI.addGridBagComponent(parkingRatesPanel, parkingRateLbl, GridBagConstraints.BOTH, 0, 0);
+
 		List<ParkingRate> parkingRates = new ArrayList<ParkingRate>();
-		parkingRates = ps.getParkingRates();
+		parkingRates = mainController.parkingController.getParkingRates();
 		DefaultTableModel model = new DefaultTableModel(new Object[][] {}, new String[] { "Hours", "Rate" });
 		for (ParkingRate pr : parkingRates) {
 			model.addRow(new Object[] { pr.getHours(), pr.getRate() });
@@ -70,27 +65,39 @@ public class EnterParkingUI {
 		parkingRatesTbl.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		parkingRatesTbl.setFillsViewportHeight(true);
 
-		parkingRatesPanel.add(parkingRatesTbl);
-		mainUI.addComponent(enterParkingMainPanel, parkingRatesPanel, GridBagConstraints.BOTH, 0, 0);
+		mainUI.addGridBagComponent(parkingRatesPanel, parkingRatesTbl, GridBagConstraints.BOTH, 0, 1);
+		mainUI.addGridBagComponent(mainContentPnl, parkingRatesPanel, GridBagConstraints.BOTH, 0, 0);
+
+		getTicketBtn.addActionListener(new EnterParkingUIListener());
+		backButton.addActionListener(new EnterParkingUIListener());
+
+		mainUI.addGridBagComponent(mainContentPnl, getTicketBtn, GridBagConstraints.NONE, 0, 1);
+		mainUI.addGridBagComponent(mainContentPnl, backButton, GridBagConstraints.NONE, 0, 2);
+		mainContentPnl.setVisible(false);
+
+		mainUI.addGridBagComponent(mainUI.mainPnl, mainContentPnl, GridBagConstraints.BOTH, 0, 0);
 	}
 
-	public class EnterParkingListener implements ActionListener {
-		EnterParkingListener() {
+	private class EnterParkingUIListener implements ActionListener {
+
+		EnterParkingUIListener() {
 		}
 
 		public void actionPerformed(ActionEvent e) {
 			if (e.getActionCommand().equals("Get Ticket")) {
-				int ticketNumber = ps.enterParking();
-				if(ticketNumber == 0) {
-					mainUI.setMessage("Sorry. Parking not available.");
-				}
-				else {
-					mainUI.setMessage("Ticket Number: " + ticketNumber);
-				}
-				
+				// Ticket ticket = new Ticket();
+				int ticket = mainController.parkingController.getTicket();
+				mainController.parkingController.openEntryGate();
+				JOptionPane.showMessageDialog(mainUI, "Ticket Number: " + ticket + "\nEntry Gate Opened. Click OK to close it.",
+						"Entry Gate opened.", JOptionPane.INFORMATION_MESSAGE);
+				mainController.parkingController.closeEntryGate();
+				mainUI.showHideContentPanel(mainUI.mainContentPnl, mainContentPnl);
+				mainUI.displayWelcomeMessage();
+			}
+			if (e.getActionCommand().equals("Go Back")) {
+				mainUI.showHideContentPanel(mainUI.mainContentPnl, mainContentPnl);
+				mainUI.displayWelcomeMessage();
 			}
 		}
-
 	}
-
 }
