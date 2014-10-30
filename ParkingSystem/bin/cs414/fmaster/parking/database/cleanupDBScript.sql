abeb6efa0812f5fb2c90c25f665a6bee83213acc
masterf89 <farheenmaster@gmail.com>
2014-10-29 23:31:26 -0400

    changes + test plan

---
+++ ParkingSystem/bin/cs414/fmaster/parking/database/cleanupDBScript.sql
@@ -0,0 +1,8 @@
+drop table audit;
+drop table accounts;
+drop table parking_rates;
+drop table parking_size_history;
+drop table payments;
+drop table tickets;
+drop table parking_availability_history;
+drop table payment_exceptions;
